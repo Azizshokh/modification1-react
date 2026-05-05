d8632064381e9011a972ddb8339ee2cb0a64acdb
ali <erali.jurakulov@mail.ru>
2026-05-06 02:17:59 +0900

    feat: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,28 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
-import "../css/app.css";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { Footer } from "./components/footer";
+import "../css/app.css";
 
 function App() {
+  const location = useLocation();
+  console.log("Current location:", location.pathname);
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link to="/member-page">Member Page</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Routes>
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/member-page" element={<UserPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
-    </div>
+      <Footer />
+    </>
   );
 }
 
