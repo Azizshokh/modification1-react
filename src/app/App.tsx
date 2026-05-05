83fa0ebfd90431396eacf192b2944432c9a0b26a
ali <erali.jurakulov@mail.ru>
2026-05-06 01:56:37 +0900

    fix: develop PetFood screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { HomePage } from "./screens/homePage";
 import "../css/app.css";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders</Link>
+          </li>
+          <li>
+            <Link to="/member-page">Member Page</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/member-page" element={<UserPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
