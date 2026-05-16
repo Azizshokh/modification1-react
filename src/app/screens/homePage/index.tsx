e327b9a14ca9553983ba53f47cc92d8f21d19ccb
ali <erali.jurakulov@mail.ru>
2026-05-17 05:07:17 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,7 +7,25 @@ import ActiveUsers from "./ActiveUsers";
 import PopularProducts from "./PopularProducts";
 import Events from "./Events";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularProducts } from "./slice";
+import { retrievePopularProducts } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+/*** Redux slice & Selector ***/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularProducts: (data: Product[]) => dispatch(setPopularProducts(data)),
+});
+const popularProductsRetriever = createSelector(
+  retrievePopularProducts,
+  (popularProducts) => ({ popularProducts }),
+);
+
 export default function HomeScreen(): React.JSX.Element {
+  const { setPopularProducts } = actionDispatch(useDispatch());
+  const popularProducts = useSelector(retrievePopularProducts);
   // Selector: Store => Data
 
   useEffect(() => {
